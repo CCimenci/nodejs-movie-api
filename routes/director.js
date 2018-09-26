35d9a601bdfed1038c5e678cf25ec6370e8eb9e2
Çağrı ÇİMENCİ <cagricimenci@arcticsofttech.com>
2018-09-26 22:29:14 +0300

    /api/directors/:director_id [GET]

--- routes/director.js
+++ routes/director.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
 
 //models
 
@@ -62,6 +63,63 @@ router.get('/', (req, res) => {
         res.json(err);
     });
 });
+
+router.get('/:director_id', (req, res) => {
+    const promise = Director.aggregate([
+        {
+            $match: {
+                '_id': mongoose.Types.ObjectId(req.params.director_id)
+            }
+        },
+        {
+            $lookup: {
+                from: 'movies',
+                localField: '_id',
+                foreignField: 'director_id',
+                as: 'movies'
+            }
+        },
+        {
+            $unwind: {
+                path: '$movies',
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $group: {
+                _id: {
+                    _id: '$_id',
+                    name: '$name',
+                    surname: '$surname',
+                    bio: '$bio'
+                },
+                movies: {
+                    $push: '$movies'
+                }
+            }
+        },
+        {
+            $project: {
+                _id: '$_id._id',
+                name: '$_id.name',
+                surname: '$_id.surname',
+                movies: '$movies'
+            }
+        }
+    ]);
+
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+
+
+
+
+
 
 
 

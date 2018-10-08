5e415b660f82b0fce2b85d6f34bb473e0f0e1d71
CCimenci <cagricimenci@gmail.com>
2018-10-08 22:34:25 +0300

    json web token verify

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.set('api_secret_key', config.api_secret_key);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//middleware
+
+const verifyToken = require('./middleware/verify-token')
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,6 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api', verifyToken);
 app.use('/api/movies', movie);
 app.use('/api/directors', director);
 
